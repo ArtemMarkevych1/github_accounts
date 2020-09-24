44512f6bd051f914c4033337ecbb44e5f7e2ee29
amarkevych <amarkevych@osdb.io>
2020-09-24 11:34:20 +0300

    Updates

---
+++ precache-manifest.981577c08cb1855ac0dee6f028f9b7a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83410c4f44377087b7602d5df434fd8",
+    "url": "/github_accounts/index.html"
+  },
+  {
+    "revision": "30e315b5b5413abfa998",
+    "url": "/github_accounts/static/css/main.b6fd2ab8.chunk.css"
+  },
+  {
+    "revision": "66b25e01d0a71a42452a",
+    "url": "/github_accounts/static/js/2.1afe8104.chunk.js"
+  },
+  {
+    "revision": "30e315b5b5413abfa998",
+    "url": "/github_accounts/static/js/main.7a8e5665.chunk.js"
+  },
+  {
+    "revision": "16d8c01b1dc5443a7b3f",
+    "url": "/github_accounts/static/js/runtime~main.e38d11b7.js"
+  }
+]);
